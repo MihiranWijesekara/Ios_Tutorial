8c2aab50c85a842436b2d539adf6055c0ca6e334
MihiranWijesekara <achinthamihiran654@gmail.com>
2026-06-13 15:46:35 +0530

    Initial Commit

---
+++ ios_development/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_development
+//
+//  Created by student5 on 2026-06-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_development/ios_developmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_developmentApp.swift
+//  ios_development
+//
+//  Created by student5 on 2026-06-13.
+//
+
+import SwiftUI
+
+@main
+struct ios_developmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
