5c21bd7c3f7f6ec5bbf8e81cb85fc64cced207fc
MihiranWijesekara <achinthamihiran654@gmail.com>
2026-07-09 15:26:18 +0530

    fix the Quiz Rush game error

---
+++ New Group/ios_development/Views/Games/QuizRushContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizRushContainerView.swift
+//  ios_development
+//
+//  Created by student5 on 2026-07-09.
+//
+
+import SwiftUI
+
+struct QuizRushContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizRushContainerView()
+}
